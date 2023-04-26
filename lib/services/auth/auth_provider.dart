421af46db71f320575b2aa724c9e058467b73250
bchizi <leonbichanga@gmail.com>
2023-04-26 17:31:43 +0300

    add logic to create user

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -2,11 +2,18 @@ import 'package:notetaker/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
   AuthUser? get currentUser;
-  Future<AuthUser> logIn({required String email, required String password});
+
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
   Future<AuthUser> createUser({
     required String email,
     required String password,
   });
+
   Future<void> logOut();
+
   Future<void> sendEmailVerification();
 }
